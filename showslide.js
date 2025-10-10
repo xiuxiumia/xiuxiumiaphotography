9149ad6b94129689be8b18e6666824e4561f097e
xiuxiumia <87248292+xiuxiumia@users.noreply.github.com>
2025-10-10 18:18:02 -0400

    Update showslide.js

--- showslide.js
+++ showslide.js
@@ -1,13 +1,21 @@
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+showSlidesManual(slideIndex);
 
-function showSlides() {
-  let slides = document.getElementsByClassName("mySlides");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.opacity = 0;
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) { slideIndex = 1 }
-  slides[slideIndex - 1].style.opacity = 1;
-  setTimeout(showSlides, 5000); // ← this number controls speed
+// Next/previous controls
+function plusSlides(n) {
+  showSlidesManual(slideIndex += n);
 }
+
+// Show the current slide
+function showSlidesManual(n) {
+  const slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = 0; // hide all slides
+  }
+  slides[slideIndex - 1].style.opacity = 1; // show current slide
+}
+
+// Auto-slide every 5 seconds
+setInterval(() => plusSlides(1), 5000);
